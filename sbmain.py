3a82ca5c99012f41bca9e162213e2363b21c36f4
yizhoucc <yizhouc@nyu.edu>
2020-03-12 14:29:55 -0500

    init

--- sbmain.py
+++ sbmain.py
@@ -4,6 +4,17 @@ from stable_baselines import A2C
 from FireflyEnv import ffenv
 
 env=gym.make('FF-v0')
+
+
+
+
+
+
+
+
+
+
+
 model = A2C(MlpPolicy, env, verbose=1)
 model.learn(total_timesteps=50000)
 

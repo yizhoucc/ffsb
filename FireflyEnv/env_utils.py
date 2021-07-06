b51f9d6bb811f24206d846d64fbb6ddb48a21692
yizhou <yizhoucc@gmail.com>
2021-07-05 23:06:50 -0500

    update

--- FireflyEnv/env_utils.py
+++ FireflyEnv/env_utils.py
@@ -38,15 +38,15 @@ def is_pos_def(x):
     """
     if x.is_cuda:
         x = x.clone().detach().cpu()
-        yes=torch.all(torch.linalg.eig(x)[0] >= torch.tensor([0.]))
+        yes=torch.all(torch.eig(x)[0] >= torch.tensor([0.]))
         if not yes:
-            print(torch.linalg.eig(x))
+            print(torch.eig(x))
     else:
         # x = x.detach().numpy()
         # yes=np.all(np.linalg.eigvalsh(x) >= 0)
-        yes=torch.all(torch.linalg.eig(x)[0] >= torch.tensor([0.]))
+        yes=torch.all(torch.eig(x)[0] >= torch.tensor([0.]))
         if not yes:
-            print(torch.linalg.eig(x))
+            print(torch.eig(x))
 
     return yes
 
